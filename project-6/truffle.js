0ec4d746c2471f8dfa36bb5bb33ac634e1241b74
marcin-kupiec <marcin.kupiec@brainly.com>
2022-07-20 18:36:45 +0200

    fix truffle config

--- project-6/truffle.js
+++ project-6/truffle.js
@@ -14,7 +14,8 @@ module.exports = {
     rinkeby: {
       provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
       network_id: 4,       // Rinkeby's id
-      gas: 1500000,        // Rinkeby has a lower block limit than mainnet
+      gas: 4500000,       // Rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000,
       // confirmations: 1,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
